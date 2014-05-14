38abf5a79eadc5b540165b771faa63b4f52f195c
Quinn Shanahan <q.shanahan@gmail.com>
2014-05-14 17:10:26 -0400

    add fonts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140509220346) do
+ActiveRecord::Schema.define(version: 20140514211004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20140509220346) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
 end
