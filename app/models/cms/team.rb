b716f1419a6e8c1811a1b0e94276d4e69b229732
Quinn Shanahan <q.shanahan@gmail.com>
2014-05-15 12:58:06 -0400

    no longer parse name

--- app/models/cms/team.rb
+++ app/models/cms/team.rb
@@ -2,10 +2,6 @@ class CMS::Team < ActiveRecord::Base
   self.table_name = 'teams'
   has_many :pages, class_name: 'CMS::Page'
 
-  def name= name
-    self[:name] = name.parameterize
-  end
-
   def self.name
     'Team'
   end
